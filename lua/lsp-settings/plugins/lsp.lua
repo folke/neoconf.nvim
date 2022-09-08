200c6fa9362cdab97177a0e6b2e3068586c8a65d
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-09-08 11:58:35 +0200

    feat: only apply configuration when it actually changed

--- lua/lsp-settings/plugins/lsp.lua
+++ lua/lsp-settings/plugins/lsp.lua
@@ -19,7 +19,7 @@ function M.setup()
   end)
 end
 
-local function merge_config(config, root_dir)
+function M.on_new_config(config, root_dir)
   if config._lsp_settings then
     config.settings = vim.deepcopy(config._lsp_settings)
   else
@@ -34,17 +34,7 @@ local function merge_config(config, root_dir)
   )
 end
 
-function M.on_new_config(config, root_dir)
-  merge_config(config, root_dir)
-end
-
--- TODO: add check if config changed
 function M.on_update(fname)
-  fname = Util.fqn(fname)
-
-  -- clear cached settings for this file
-  Settings.clear(fname)
-
   local root_dir = Util.fqn(vim.fn.fnamemodify(fname, ":h"))
 
   local is_global = Util.is_global(fname)
@@ -54,20 +44,25 @@ function M.on_update(fname)
   for _, client in ipairs(clients) do
     -- reload this client if the global file changed, or its root dir equals the local one
     if is_global or client.config.root_dir == root_dir then
+      local old = vim.deepcopy(client.config.settings)
+
       -- re-apply config from any other plugins that were overriding on_new_config
       if client.config.on_new_config then
         pcall(client.config.on_new_config, client.config, client.config.root_dir)
       end
 
-      -- notify the lsp server of thr new config
-      local ok = pcall(client.notify, "workspace/didChangeConfiguration", {
-        settings = client.config.settings,
-      })
+      -- only send update when confiuration actually changed
+      if not vim.deep_equal(old, client.config.settings) then
+        -- notify the lsp server of thr new config
+        local ok = pcall(client.notify, "workspace/didChangeConfiguration", {
+          settings = client.config.settings,
+        })
 
-      if ok then
-        Util.info("Reloaded settings for " .. client.name)
-      else
-        Util.error("Reloading settings failed for " .. client.name)
+        if ok then
+          Util.info("Reloaded settings for " .. client.name)
+        else
+          Util.error("Reloading settings failed for " .. client.name)
+        end
       end
     end
   end
