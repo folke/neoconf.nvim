c6f75787e7870d295b12cebf83e5b9f197f3e037
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-09-09 23:21:46 +0200

    feat: added settings.nvim config schema

--- lua/settings/plugins/init.lua
+++ lua/settings/plugins/init.lua
@@ -22,7 +22,25 @@ function M.register(plugin)
 end
 
 function M.setup()
-  M.register(require("settings.plugins.lsp"))
+  M.register({
+    get_schema = function()
+      local defaults = require("settings.config").defaults
+      local ret = require("settings.schema").plugin_schema("settings.nvim", defaults, "Settings for settings.nvim")
+      ret.properties["settings.nvim"].properties.plugins.properties.sumneko_lua = {
+        type = "object",
+        properties = {
+          enabled = {
+            description = "When null, this will only be enabled for your neovim config",
+            type = {
+              "boolean",
+              "null",
+            },
+          },
+        },
+      }
+      return ret
+    end,
+  })
   M.register(require("settings.plugins.lspconfig"))
   M.register(require("settings.plugins.jsonls"))
   M.register(require("settings.plugins.sumneko"))
