944333cda4894e9378bf822d966ff8f9debb8676
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-09-09 23:21:28 +0200

    feat: added config options for all plugins

--- lua/settings/config.lua
+++ lua/settings/config.lua
@@ -4,8 +4,21 @@ local M = {}
 M.defaults = {
   local_settings = { ".nvim.settings.json", vscode = ".vscode/settings.json" },
   global_settings = { "nvim.settings.json" },
-  jsonls = {
-    configured_servers_only = true, -- only show completion in json settings for configured lsp servers
+  plugins = {
+    lspconfig = {
+      enabled = true,
+    },
+    -- configures jsonls to get completion in .nvim.settings files
+    jsonls = {
+      enabled = true,
+      configured_servers_only = true, -- only show completion in json settings for configured lsp servers
+    },
+    -- configures sumneko_lua to get completion of lspconfig server settings
+    sumneko_lua = {
+      -- nil: by default, this is only enabled in your neovim config directory
+      -- true/false: you can explicitely enable/disable this by settings the property below
+      enabled = nil,
+    },
   },
 }
 
@@ -19,6 +32,10 @@ end
 ---@return Config
 function M.merge(options)
   return vim.tbl_deep_extend("force", {}, M.options, options or {})
+end
+
+function M.get(opts)
+  return require("settings").get("settings.nvim", M.options, opts)
 end
 
 return M
